661fd6507dac499984082ba740c2322cfe6a7f4d
risto <risto.kowaczewski@gmail.com>
2024-04-23 10:10:46 +0200

    Version 0.9.1-dev

--- test/unit/test_version.py
+++ test/unit/test_version.py
@@ -2,5 +2,5 @@ from tracardi.domain.version import Version
 
 
 def test_version_name():
-    version = Version(version="0.9.0-dev", db_version="090x")
-    assert version.name == Version._generate_name("090x")
+    version = Version(version="0.9.1-dev", db_version="091x")
+    assert version.name == Version._generate_name("091x")
