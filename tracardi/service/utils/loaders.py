79a015031823a589d0af03d892d67cbaf6a52508
risto <risto.kowaczewski@gmail.com>
2024-09-23 22:59:07 +0200

    Adds aliases to tenants.

--- tracardi/service/utils/loaders.py
+++ tracardi/service/utils/loaders.py
@@ -28,24 +28,3 @@ def pre_config_file_loader(file_path):
             return None
     logger.info(f"Preconfiguration file `{file_path}` loaded with {len(content) if content else 'no'} definitions.")
     return content
-
-
-def load_file_to_dict(file_path: str) -> dict[str, str]:
-    my_dict = {}
-
-    if not os.path.exists(file_path):
-        return my_dict
-
-    # Open the file in read mode
-    with open(file_path, 'r') as file:
-        for line in file:
-            # Strip any leading/trailing whitespace and split only on the first colon
-            key_value = line.strip().split(':', 1)
-
-            if len(key_value) == 2:
-                key, value = key_value
-                my_dict[key.strip()] = value.strip()
-            else:
-                logger.error(f"Skipping invalid line in tenant-aliases.txt: {line.strip()}")
-
-    return my_dict
